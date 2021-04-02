2c155e0e7d0cddffc8707bf951720e3db0e0e8dd
dneumann <dustinneumann42@gmail.com>
2021-04-01 17:22:09 -0700

    stubbed repl

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <sstream>
+#include <string>
+
+#include "vault.hpp"
+
+std::string readInput() {
+  std::string result{""}; 
+  std::getline(std::cin, result); 
+  return result;
+}
+
+Vault::Status repl() {
+  std::cout << "Vault (" << VAULT_VERSION << ")\n";
+
+  bool running = true;
+  while (running) { 
+    // Read
+    std::cout << VAULT_PROMPT;
+
+    std::string code = readInput();
+
+    // Eval
+
+    // Print
+  }
+
+  return Vault::Status::SUCCESS;
+}
 
 int main(const int num_args, const char* args[]) {
-  
+  if (num_args == 1) { return repl(); }
+
   return EXIT_SUCCESS;
 }
